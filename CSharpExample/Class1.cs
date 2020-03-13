c3f1993c120953f4873eb29f5441e9ff69e332e0
staceyloulouse <staceyloulouse@gmail.com>
2020-03-13 08:07:55 -0400

    Add project files.

---
+++ CSharpExample/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CSharpExample
+{
+    [Serializable()]
+    public class Animal : ISerializable
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+
+        public int AnimalID { get; set; }
+        public Animal() { }
+        public Animal(string name = "No Name",
+            double weight = 0,
+            double height = 0)
+        {
+            Name = name;
+            Weight = weight;
+            Height = height;
+        }
+        public Animal(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Weight = (double)info.GetValue("Weight", typeof(double));
+            Height = (double)info.GetValue("Height", typeof(double));
+
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} weighs {1} lbs and is {2} inches tall", Name, Weight, Height);
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            //throw new NotImplementedException();
+            info.AddValue("Name", Name);
+            info.AddValue("Weight", Weight);
+            info.AddValue("Height", Height);
+
+        }
+    }
+}
---
+++ CSharpExample/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "AnimalData.dat";
+            string xmlPath = "C:/Users/stace/source/repos/CSharpExample/CSharpExample/bowser.xml";
+            string xmlPath2 = "C:/Users/stace/source/repos/CSharpExample/CSharpExample/animals.xml";
+
+            Animal bowser = new Animal("Bowser", 45,25);
+            Stream stream = File.Open(path, FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, bowser);
+            stream.Close();
+
+            bowser = null;
+            stream = File.Open(path, FileMode.Open);
+            bowser = (Animal)bf.Deserialize(stream);
+            stream.Close();
+            Console.WriteLine(bowser.ToString());
+
+            bowser.Weight = 50;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Animal));
+            using(TextWriter tw = new StreamWriter(xmlPath))
+            {
+                serializer.Serialize(tw, bowser);
+            }
+            bowser = null;
+            XmlSerializer deserializer =new XmlSerializer(typeof(Animal));
+            TextReader reader = new StreamReader(xmlPath);
+            object obj = deserializer.Deserialize(reader);
+            bowser = (Animal)obj;
+            reader.Close();
+
+            Console.WriteLine(bowser.ToString());
+            //save a collection of Animals
+            List<Animal> theAnimals = new List<Animal>
+            {
+                new Animal("Mario",60, 30),
+                new Animal("Luigi",55, 24),
+                new Animal("Peach",40, 20)
+            };
+
+            using (Stream fs = new FileStream(xmlPath2, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer serializer2 = new XmlSerializer(typeof(List<Animal>));
+                serializer2.Serialize(fs, theAnimals);
+            }
+            theAnimals = null;
+            XmlSerializer serializer3 = new XmlSerializer(typeof(List<Animal>));
+
+            using (FileStream fs2 = File.OpenRead(@xmlPath2))
+            {
+                theAnimals = (List<Animal>)serializer3.Deserialize(fs2);
+            }
+
+            foreach (Animal a in theAnimals)
+            {
+                Console.WriteLine(a.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
